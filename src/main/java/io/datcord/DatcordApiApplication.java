c9f68b516006fe1fd034280420cd6dc68fc2417f
Quinton <149415184+quinton-k@users.noreply.github.com>
2024-06-24 12:19:36 -0600

    Initial Commit

---
+++ src/main/java/io/datcord/DatcordApiApplication.java
@@ -0,0 +1,13 @@
+package io.datcord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatcordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatcordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/datcord/DatcordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datcord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatcordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
